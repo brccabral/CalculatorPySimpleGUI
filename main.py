ec859dbc3b62ca32f039d7641b5e3aec383c94d9
brccabral <cabral.brc@gmail.com>
2022-04-04 20:37:36 -0700

    initial layout

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 import PySimpleGUI as sg
 
 
-layout = [[]]
+layout = [
+    [sg.Text("output", key="-OUTPUT-")],
+    [sg.Button("Clear", key="-CLEAR-"), sg.Button("Enter", key="-ENTER-")],
+    [
+        sg.Button("7", key="-7-"),
+        sg.Button("8", key="-8-"),
+        sg.Button("9", key="-9-"),
+        sg.Button("*", key="-*-"),
+    ],
+    [
+        sg.Button("4", key="-4-"),
+        sg.Button("5", key="-5-"),
+        sg.Button("6", key="-6-"),
+        sg.Button("/", key="-/-"),
+    ],
+    [
+        sg.Button("1", key="-1-"),
+        sg.Button("2", key="-2-"),
+        sg.Button("3", key="-3-"),
+        sg.Button("-", key="---"),
+    ],
+    [sg.Button("0", key="-0-"), sg.Button(".", key="-.-"), sg.Button("+", key="-+-")],
+]
 
 window = sg.Window("Title", layout)
 
