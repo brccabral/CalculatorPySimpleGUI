2a8277eed509fafb7bf3028f2355cfeb1d511678
Bruno Cabral <cabral.brc@gmail.com>
2022-04-04 17:23:08 -0700

    added button size

--- main.py
+++ main.py
@@ -4,30 +4,38 @@ import PySimpleGUI as sg
 sg.theme("LightGrey1")
 print(sg.theme_list())  # prints all themes
 
-sg.set_options(font="Franklin 14")
+button_size = (6, 3)
+sg.set_options(font="Franklin 14", button_element_size=button_size)
 
 layout = [
     [sg.Text("output", key="-OUTPUT-")],
-    [sg.Button("Clear", key="-CLEAR-"), sg.Button("Enter", key="-ENTER-")],
     [
-        sg.Button("7", key="-7-"),
-        sg.Button("8", key="-8-"),
-        sg.Button("9", key="-9-"),
-        sg.Button("*", key="-*-"),
+        sg.Button("Clear", key="-CLEAR-", expand_x=True),
+        sg.Button("Enter", key="-ENTER-", expand_x=True),
     ],
     [
-        sg.Button("4", key="-4-"),
-        sg.Button("5", key="-5-"),
-        sg.Button("6", key="-6-"),
-        sg.Button("/", key="-/-"),
+        sg.Button("7", key="-7-", size=button_size),
+        sg.Button("8", key="-8-", size=button_size),
+        sg.Button("9", key="-9-", size=button_size),
+        sg.Button("*", key="-*-", size=button_size),
     ],
     [
-        sg.Button("1", key="-1-"),
-        sg.Button("2", key="-2-"),
-        sg.Button("3", key="-3-"),
-        sg.Button("-", key="---"),
+        sg.Button("4", key="-4-", size=button_size),
+        sg.Button("5", key="-5-", size=button_size),
+        sg.Button("6", key="-6-", size=button_size),
+        sg.Button("/", key="-/-", size=button_size),
     ],
-    [sg.Button("0", key="-0-"), sg.Button(".", key="-.-"), sg.Button("+", key="-+-")],
+    [
+        sg.Button("1", key="-1-", size=button_size),
+        sg.Button("2", key="-2-", size=button_size),
+        sg.Button("3", key="-3-", size=button_size),
+        sg.Button("-", key="---", size=button_size),
+    ],
+    [
+        sg.Button("0", key="-0-", expand_x=True),
+        sg.Button(".", key="-.-", size=button_size),
+        sg.Button("+", key="-+-", size=button_size),
+    ],
 ]
 
 window = sg.Window("Title", layout)
